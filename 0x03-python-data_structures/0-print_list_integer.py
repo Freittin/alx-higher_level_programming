0785c8c90fd544ff63c4b24fe7c315b21e71ad94
freittin <nideologies@gmail.com>
2022-09-26 12:34:21 -0700

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print(i)
+
+
+if __name__ == "__main__":
+    print_list_integer
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+this is on data structures in python
