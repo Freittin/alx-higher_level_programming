feac77306a822a879e3642584920440f799aa2f1
freittin <nideologies@gmail.com>
2022-10-26 08:21:49 -0700

    python input/output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' Module for from_json_string func '''
+import json
+
+
+def from_json_string(my_str):
+    ''' returns an object represented by a JSON string '''
+    return json.loads(my_str)
