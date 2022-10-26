23e7a5f60f1ba5d0139987a4e398572a40e045a2
freittin <nideologies@gmail.com>
2022-10-26 07:06:11 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Module for lookup method.'''
+
+
+def lookup(obj):
+    '''Looks up object attributes and methods.
+    Args:
+        obj (object): the object to list.
+    Returns:
+       list: the list of attributes.
+    '''
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is 0x0A. Python - Inheritance
